83fabafa549a7ad40d1eb1510747762fc1fc0d48
Jaewon Han <jaewonhan20@gmail.com>
2023-09-22 16:43:40 -0500

    chore: Install react-router-dom and basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./pages/Root";
+import Coin from "./pages/Coin";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/coin",
+        element: <Coin />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
