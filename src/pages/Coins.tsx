5606fc7182033c54a48ec5c5a216a7f3038cb018
Jaewon Han <jaewonhan20@gmail.com>
2023-09-22 23:02:47 -0500

    feat: Add theme and types, fetch and display coin data

--- src/pages/Coins.tsx
+++ src/pages/Coins.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Outlet, Link } from "react-router-dom";
 
 const Container = styled.div`
-  padding: 0px 20px;
+  padding: 0px 10px;
+  max-width: 480px;
+  margin: 0 auto;
 `;
 const Header = styled.header`
   height: 10vh;
@@ -15,62 +17,66 @@ const CoinsList = styled.ul``;
 const Coin = styled.li`
   background-color: #ffdad9;
   margin-bottom: 10px;
-  padding: 20px;
+  color: ${(props) => props.theme.bgColor};
+  padding: 10px;
   border-radius: 15px;
   a {
     padding: 20px;
-    transition: color 0.2s ease-in;\
+    transition: color 0.2s ease-in;
     display: block;
   }
   &:hover {
-    color: blue;
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
   }
 `;
 const Title = styled.h1`
   font-size: 50px;
+  color: ${(props) => props.theme.accentColor};
 `;
-const coins = [
-  {
-    id: "btc-bitcoin",
-    name: "Bitcoin",
-    symbol: "BTC",
-    rank: 1,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "eth-ethereum",
-    name: "Ethereum",
-    symbol: "ETH",
-    rank: 2,
-    is_new: false,
-    is_active: true,
-    type: "coin",
-  },
-  {
-    id: "hex-hex",
-    name: "HEX",
-    symbol: "HEX",
-    rank: 3,
-    is_new: false,
-    is_active: true,
-    type: "token",
-  },
-];
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
 export default function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
   return (
     <Container>
       <Header>
         <Title>Coins</Title>
       </Header>
-      <CoinsList>
-        {coins.map((coin) => (
-          <Coin key={coin.id}>
-            <Link to={`${coin.name}`}>{coin.name} &rarr;</Link>
-          </Coin>
-        ))}
-      </CoinsList>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link to={`${coin.name}`}>{coin.name} &rarr;</Link>
+            </Coin>
+          ))}
+        </CoinsList>
+      )}
       <Outlet />
     </Container>
   );
